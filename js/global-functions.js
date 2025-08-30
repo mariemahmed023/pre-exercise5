08354cfda79e46df3719b54f608c92a2feaf4af5
mariemahmed023 <maryamahmedelsayedshatah124@gmail.com>
2025-08-30 15:05:43 +0300

    Add files via upload

--- js/global-functions.js
+++ js/global-functions.js
@@ -12,7 +12,7 @@ function getStudent(currentid) {
 }
 
 function showStudent(student) {
-    tbody.innerHTML += `<tr>
+    tbody.innerHTML += `<tr data-student-id="${student.id}">
                                 <th scope="row">${student.id}</th>
                                 <td>${student.firstName}</td>
                                 <td>${student.lastName}</td>
@@ -20,10 +20,10 @@ function showStudent(student) {
                                 <td>${student.age}</td>
                                 <td>${student.phone}</td>
                                 <td>
-                                    <button class="btn btn-info text-light me-2">
+                                    <button class="btn btn-info text-light me-2" onclick="editStudent(${student.id})">
                                         Edit
                                     </button>
-                                    <button class="btn btn-danger text-light">
+                                    <button class="btn btn-danger text-light" onclick="deleteStudent(${student.id}, this)">
                                         Delete
                                     </button>
                                 </td>
@@ -36,33 +36,77 @@ function checkInput(input) {
         inputAlert = input.parentElement.nextElementSibling,
         regex;
 
-    if (inputName == "firstName" ||inputName == "lastName") {
+    if (inputName == "firstName" || inputName == "lastName") {
         regex = /^[A-Za-z]{3,}$/;
     }
 
-    else if ( inputName == "email"){
+    else if (inputName == "email") {
         regex = /^[A-Za-z]+[0-9_\-\.]*@(gmail|yahoo)\.(com|org)$/;
     }
 
-    else if (inputName == "age"){
+    else if (inputName == "age") {
         regex = /^[1 - 9]{1}[0-9]?$/;
     }
 
-    else if (inputName == "phone"){
+    else if (inputName == "phone") {
         regex = /^(02)?01(0|1|2|5)[0-9]{8}$/;
     }
 
     if (inputValue == "") {
         inputAlert.textContent = "This Field is Required";
+        inputAlert.classList.remove("d-none");
+        input.setAttribute("data-valid", false);
     }
 
     else if (!regex.test(inputValue)) {
         inputAlert.textContent = "Invalid Field";
         inputAlert.classList.remove("d-none");
+        input.setAttribute("data-valid", false);
+
     }
 
     else {
         inputAlert.classList.add("d-none");
+        input.setAttribute("data-valid", true);
     }
 
+}
+
+function showAllStudents(currentStudents) {
+    currentStudents.forEach((student) => {
+        showStudent(student);
+    });
+}
+
+function updateLocalStorage(){
+    localStorage.setItem("students", JSON.stringify(students));
+    localStorage.setItem("id", JSON.stringify(id));
+}
+
+function deleteStudent(studentId, that){
+
+    if (!confirm("Are You Sure")){
+        return 0 ;
+    }
+
+    students = students.filter((student) => {return student.id != studentId});
+
+    updateLocalStorage();
+
+    that.parentElement.parentElement.remove();
+}
+
+function editStudent(studentId){
+
+    let student = students.filter((student) => {student.id === studentId})[0];
+
+    formInputs.forEach((e) =>{
+        formInput.value = student[formInput.name];
+    });
+
+    formEle.dataset.type = "edit";
+    formButton.querySelector("button").classList.replace("btn-success" , "btn-info");
+    formButton.textContent = "Edit";
+
+    formEle.setAttribute("data-student-id", student.id)
 }
--- js/index.js
+++ js/index.js
@@ -2,26 +2,64 @@ let formEle = document.querySelector("form"),
     formInputs = formEle.querySelectorAll("input"),
     id = 0,
     students = [],
-    tbody = document.querySelector("tbody");
+    tbody = document.querySelector("tbody"),
+    validInputs = [],
+    formButton = formEle.querySelector("button");
+
+    (function (){
+        if (localStorage.getItem("students") == null){
+        localStorage.setItem("students" , JSON.stringify(students));
+    }
+
+    else {
+        students = JSON.parse(localStorage.getItem("students"));
+    }
+
+    if (localStorage.getItem("id") == null){
+        localStorage.setItem("id" , id);
+    }
+
+    else {
+        id = +(localStorage.getItem("id"));
+    }
+    showAllStudents(students);
+    })();
 
     
+
 
 formEle.addEventListener("submit", function (e)  {
     e.preventDefault();
 
-    
+    if (formEle.dataset.type == "add"){
+        formEle.querySelector("input:focus").blur();
+    for ( let formInput of formInputs){
+        if (formInput.dataset.valid === "false"){
+            return 0;
+        }
+    }
 
     let newStudent = getStudent(++id);
 
 
     students.push(newStudent);
 
+    updateLocalStorage();
+
     showStudent(newStudent);
 
     this.reset();
+    }
+
+    else if (formEle.dataset.type = "edit"){
+        
+    }
 
 });
 
 for ( let formInput of formInputs){
-        checkInput(formInput);
+        formInput.addEventListener("blur",function(){
+            checkInput(formInput);
+        });
     }
+
